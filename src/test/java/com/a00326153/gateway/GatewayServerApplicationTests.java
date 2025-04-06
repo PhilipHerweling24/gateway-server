7d004cda08f5cd63c1fa881e74bde78e3e44171d
PhilipHerweling <miggymorette@gmail.com>
2025-04-06 15:56:48 +0100

    Initialised gateway

---
+++ src/test/java/com/a00326153/gateway/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a00326153.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
